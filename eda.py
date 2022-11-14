1bcde861a9de737a229abef970ae0dfa07487569
apulsarT <aggychi.apulsar@gmail.com>
2022-11-14 11:39:01 +0800

    updated

--- eda.py
+++ eda.py
@@ -8,6 +8,19 @@ import matplotlib.dates as mdates
 df_underground = pd.read_csv('data/underground.csv')
 df_weather = pd.read_csv('data/weather.csv')
 
+
+def prep(df):
+  df['DateTime']=pd.to_datetime(df['DateTime'])
+  df['DateTime'] = df['DateTime'] + timedelta(hours=8)
+  df["month"] = df['DateTime'].dt.month
+  df["day"] = df['DateTime'].dt.day
+  df["hours"] = df['DateTime'].dt.hour
+  df['minutes'] = df['DateTime'].dt.minute
+  df["date"] = df['DateTime'].dt.date  
+  df = df.groupby(['month','day','hours','minutes']).first().reset_index()  
+  
+
+
 def weatherprep(df):
   df['DateTime'] = pd.to_datetime(df['DateTime'])
   # Weather data is recorded in different time zones, all data needs to be added for 8 hours to change to the correct time zone
@@ -29,6 +42,7 @@ def weatherprep(df):
   return df
 
 df_weather = weatherprep(df_weather)
+
 
 #function to filter date
 def filterdate(df,start,end):
